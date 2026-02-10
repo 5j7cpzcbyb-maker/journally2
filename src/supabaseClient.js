c0d731ecde71ac80b4921e430ee168a7b8702a05
5j7cpzcbyb-maker <evanks032@gmail.com>
2026-02-10 15:49:17 -0800

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,7 @@
-// This file tells your app WHERE your database is located.
-import { createClient } from '@supabase/supabase-js';
 
-// Replace these two lines with the actual keys from your Supabase dashboard
-const supabaseUrl = 'https://wnxpybhtdfiqgnmxpnkr.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndueHB5Ymh0ZGZpcWdubXhwbmtyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA3NTg1NDgsImV4cCI6MjA4NjMzNDU0OH0.ubGTzI1Q6xfHI49nENjGIf8uDFvgA-qVxGz168IH_t8';
+import { createClient } from '@supabase/supabase-js'; 
+
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
