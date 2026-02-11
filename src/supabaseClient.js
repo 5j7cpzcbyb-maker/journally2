0aa2591ed2ea46d3a48b06425f64e2795d62c01b
5j7cpzcbyb-maker <evanks032@gmail.com>
2026-02-10 16:39:29 -0800

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
 
-import { createClient } from '@supabase/supabase-js'; 
-
+// These lines tell the app to look at your Vercel/Supabase settings
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
